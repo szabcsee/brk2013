9b1b9da2fae1c272875fd3bce92b8d3c00080346
Szabcsee MacBook <szabi@digitalyogis.com>
2013-04-19 13:33:11 +0200

    Rails new app generated

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Brk2013::Application.config.secret_token = 'eaaa5a24a4a2b29e3903ae6e30327729d8c9ca2a8f3267be78ea8cdeecdd3a04cf18c2aea2079d65547d8ff1d7d874a1daa49d8d7078665a8944df5560bb39f5'
