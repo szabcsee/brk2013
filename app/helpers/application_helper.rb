ac458fce31e2962b63992075cc91ac948dc4b95c
Lajos Fülep <fulep.lajos@gmail.com>
2013-05-14 15:32:01 +0200

    additional localization

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,14 @@
 module ApplicationHelper
- def countries_list 
+  def countries_list 
     I18n.t(:home_country).map { |key, value| [ value, key ] } 
-end
+  end
+  def price_category_list
+    I18n.t(:price_category).map { |key, value| [ value, key ] }
+  end
+  def price_method_list
+    I18n.t(:price_method).map { |key, value| [ value, key ] }
+  end
+  def first_day_meal_type_list
+    I18n.t(:first_day_meal_type).map { |key, value| [ value, key ] }
+  end
 end
