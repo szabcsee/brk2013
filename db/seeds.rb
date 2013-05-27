7a496db1bfb250f8f84d5b00fafcf7046f3e69e9
Szabcsee MacBook <szabi@digitalyogis.com>
2013-05-27 21:41:17 +0200

    Program page added, seed.rb program dates fixed, language file entries added, gps cordinates double quote removed

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,80 +2,80 @@
 Program.delete_all
 # . . .
 Program.create(id: 1,
-  program_name_hu: 'Tommy Boggs előadása (15pm)',
+  program_name_hu: 'Tommy Boggs előadása',
   program_name_en: 'Tommy Boggs travelling teacher',
   price_full: 1200,
   price_full_eur: 5,
   price_discount_eur: 2,
   price_discount: 600,
   exchange_rate: 280,
-  event_date: DateTime.strptime("07/09/2013", "%m/%d/%Y")
+  event_date: DateTime.strptime("2013-07-09 15:00+02:00", '%Y-%m-%d %H:%M%z')
   )
 # . . .
 Program.create(id: 2,
-  program_name_hu: 'Láma Ole Nydahl tanítása (20pm)',
+  program_name_hu: 'Láma Ole Nydahl tanítása',
   program_name_en: 'Lama Ole Nydahl\'s lecture',
   price_full: 3500,
   price_full_eur: 12.5,
   price_discount_eur: 6.5,
   price_discount: 1800,
   exchange_rate: 280,
-  event_date: DateTime.strptime("07/09/2013", "%m/%d/%Y")
+  event_date: DateTime.strptime("2013-07-09 20:00+02:00", '%Y-%m-%d %H:%M%z')
   )
 # . . .
 Program.create(id: 3,
-  program_name_hu: 'Pedro Gomez előadása (15pm)',
-  program_name_en: 'Pedro Gomez travelling teacher (15pm)',
+  program_name_hu: 'Pedro Gomez előadása',
+  program_name_en: 'Pedro Gomez travelling teacher',
   price_full: 1200,
   price_full_eur: 5,
   price_discount_eur: 2,
   price_discount: 600,
   exchange_rate: 280,
-  event_date: DateTime.strptime("07/10/2013", "%m/%d/%Y")
+  event_date: DateTime.strptime("2013-07-10 15:00+02:00", '%Y-%m-%d %H:%M%z')
   )
 # . . .
 Program.create(id: 4,
-program_name_hu: 'Láma Ole Nydahl tanítása (20pm)',
+program_name_hu: 'Láma Ole Nydahl tanítása',
   program_name_en: 'Lama Ole Nydahl\'s lecture',
   price_full: '3500',
   price_full_eur: 12.5,
   price_discount_eur: 6.5,
   price_discount: '1800',
   exchange_rate: '280',
-  event_date: DateTime.strptime("07/10/2013", "%m/%d/%Y")
+  event_date: DateTime.strptime("2013-07-10 20:00+02:00", '%Y-%m-%d %H:%M%z')
   )
 # . . .
 Program.create(id: 5,
-  program_name_hu: 'Serab Gyalcen rinpocse - A kagyü vonal és a Karmapák (15pm)',
+  program_name_hu: 'Serab Gyalcen rinpocse - A kagyü vonal és a Karmapák',
   program_name_en: 'Sherab Gyaltsen Rinpoche - Teachings about the Kagyu Lineage, especially Karmapas',
   price_full: '3500',
   price_full_eur: 12.5,
   price_discount_eur: 6.5,
   price_discount: '1800',
   exchange_rate: '280',
-  event_date: DateTime.strptime("07/11/2013", "%m/%d/%Y")
+  event_date: DateTime.strptime("2013-07-11 15:00+02:00", '%Y-%m-%d %H:%M%z')
   )
 # . . .
 Program.create(id: 6,
-  program_name_hu: 'Láma Ole Nydahl tanítása (20pm)',
+  program_name_hu: 'Láma Ole Nydahl tanítása',
   program_name_en: 'Lama Ole Nydahl\'s lecture',
   price_full: '3500',
   price_full_eur: 12.5,
   price_discount_eur: 6.5,
   price_discount: '1800',
   exchange_rate: '280',
-  event_date: DateTime.strptime("07/11/2013", "%m/%d/%Y")
+  event_date: DateTime.strptime("2013-07-11 20:00+02:00", '%Y-%m-%d %H:%M%z')
   )
 # . . .
 Program.create(id: 7,
-  program_name_hu: 'Serab Gyalcen rinpocse - Tanítók a menedékfán (15pm)',
+  program_name_hu: 'Serab Gyalcen rinpocse - Tanítók a menedékfán',
   program_name_en: 'Sherab Gyaltsen Rinpoche - The Teachers in the Refuge Tree',
   price_full: '3500',
   price_full_eur: 12.5,
   price_discount_eur: 6.5,
   price_discount: '1800',
   exchange_rate: '280',
-  event_date: DateTime.strptime("07/12/2013", "%m/%d/%Y")
+  event_date: DateTime.strptime("2013-07-12 15:00+02:00", '%Y-%m-%d %H:%M%z')
   )
 # . . .
 Program.create(id: 8,
@@ -86,38 +86,38 @@ Program.create(id: 8,
   price_discount_eur: 6.5,
   price_discount: '1800',
   exchange_rate: '280',
-  event_date: DateTime.strptime("07/12/2013", "%m/%d/%Y")
+  event_date: DateTime.strptime("2013-07-12 20:00+02:00", '%Y-%m-%d %H:%M%z')
   )
 # . . .
 Program.create(id: 9,
-  program_name_hu: 'Serab Gyalcen rinpocse - Szóbeli átadas (lungok) (15pm)',
+  program_name_hu: 'Serab Gyalcen rinpocse - Szóbeli átadas (lungok)',
   program_name_en: 'Sherab Gyaltsen Rinpoche - Oral tranmission (lungs)',
   price_full: '3500',
   price_full_eur: 12.5,
   price_discount_eur: 6.5,
   price_discount: '1800',
   exchange_rate: '280',
-  event_date: DateTime.strptime("07/13/2013", "%m/%d/%Y")
+  event_date: DateTime.strptime("2013-07-13 15:00+02:00", '%Y-%m-%d %H:%M%z')
   )
 # . . .
 Program.create(id: 10,
-  program_name_hu: 'Láma Ole Nydahl tanítása (20pm)',
+  program_name_hu: 'Láma Ole Nydahl tanítása',
   program_name_en: 'Lama Ole Nydahl\'s lecture',
   price_full: '3500',
   price_full_eur: 12.5,
   price_discount_eur: 6.5,
   price_discount: '1800',
   exchange_rate: '280',
-  event_date: DateTime.strptime("07/13/2013", "%m/%d/%Y")
+  event_date: DateTime.strptime("2013-07-13 20:00+02:00", '%Y-%m-%d %H:%M%z')
   )
 # . . .
 Program.create(id: 11,
-  program_name_hu: 'Serab Gyalcen rinpocse - Karma Paksi (2. Karmapa)-beavatás (11am)',
+  program_name_hu: 'Serab Gyalcen rinpocse - Karma Paksi (2. Karmapa)-beavatás',
   program_name_en: 'Sherab Gyaltsen Rinpoche - Karmapa Pakshi (2nd Karmapa) empowerment',
   price_full: 7000,
   price_full_eur: 25,
   price_discount_eur: 12.5,
   price_discount: 3500,
   exchange_rate: 280,
-  event_date: DateTime.strptime("07/14/2013", "%m/%d/%Y")
+  event_date: DateTime.strptime("2013-07-14 11:00+02:00", '%Y-%m-%d %H:%M%z')
   )
