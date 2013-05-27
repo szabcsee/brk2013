a13d65a0843e8b19ea6b8821060ba83ccfe0837c
Lajos Fülep <fulep.lajos@gmail.com>
2013-05-27 20:27:23 +0200

    fixing the program list

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,9 @@
+# encoding: UTF-8
 Program.delete_all
 # . . .
 Program.create(id: 1,
-  program_name_hu: 'Utazotanito (Tommy B.) - tanitas napkozben',
-  program_name_en: 'Traveling teacher (Tommy B.) - teaching during the day',
+  program_name_hu: 'Tommy Boggs előadása (15pm)',
+  program_name_en: 'Tommy Boggs travelling teacher',
   price_full: 1200,
   price_full_eur: 5,
   price_discount_eur: 2,
@@ -12,8 +13,8 @@ Program.create(id: 1,
   )
 # . . .
 Program.create(id: 2,
-  program_name_hu: 'Lama Ole Nydahl - tanitas este',
-  program_name_en: 'Lama Ole Nydahl - evening lecture',
+  program_name_hu: 'Láma Ole Nydahl tanítása (20pm)',
+  program_name_en: 'Lama Ole Nydahl\'s lecture',
   price_full: 3500,
   price_full_eur: 12.5,
   price_discount_eur: 6.5,
@@ -23,8 +24,8 @@ Program.create(id: 2,
   )
 # . . .
 Program.create(id: 3,
-  program_name_hu: 'Utazotanito (Pedro) - tanitas napkozben',
-  program_name_en: 'Travelling teacher (Pedro) - teaching during the day',
+  program_name_hu: 'Pedro Gomez előadása (15pm)',
+  program_name_en: 'Pedro Gomez travelling teacher (15pm)',
   price_full: 1200,
   price_full_eur: 5,
   price_discount_eur: 2,
@@ -34,8 +35,8 @@ Program.create(id: 3,
   )
 # . . .
 Program.create(id: 4,
-  program_name_hu: 'Lama Ole Nydahl - tanitas este',
-  program_name_en: 'Lama Ole Nydahl - evening lecture',
+program_name_hu: 'Láma Ole Nydahl tanítása (20pm)',
+  program_name_en: 'Lama Ole Nydahl\'s lecture',
   price_full: '3500',
   price_full_eur: 12.5,
   price_discount_eur: 6.5,
@@ -45,8 +46,8 @@ Program.create(id: 4,
   )
 # . . .
 Program.create(id: 5,
-  program_name_hu: 'Sherab Gyaltsen Rinpoche - A kagyu vonal es a Karmapak',
-  program_name_en: 'Lama Sherab Gyaltsen Rinpoche - Teachings about the Kagyu Lineage, especially Karmapas',
+  program_name_hu: 'Serab Gyalcen rinpocse - A kagyü vonal és a Karmapák (15pm)',
+  program_name_en: 'Sherab Gyaltsen Rinpoche - Teachings about the Kagyu Lineage, especially Karmapas',
   price_full: '3500',
   price_full_eur: 12.5,
   price_discount_eur: 6.5,
@@ -56,8 +57,8 @@ Program.create(id: 5,
   )
 # . . .
 Program.create(id: 6,
-  program_name_hu: 'Lama Ole Nydahl - tanitas este',
-  program_name_en: 'Lama Ole Nydahl - evening lecture',
+  program_name_hu: 'Láma Ole Nydahl tanítása (20pm)',
+  program_name_en: 'Lama Ole Nydahl\'s lecture',
   price_full: '3500',
   price_full_eur: 12.5,
   price_discount_eur: 6.5,
@@ -67,8 +68,8 @@ Program.create(id: 6,
   )
 # . . .
 Program.create(id: 7,
-  program_name_hu: 'Sherab Gyaltsen Rinpoche - A menedekfa tanitoi',
-  program_name_en: 'Lama Sherab Gyaltsen Rinpoche - The Teachers in the Refuge Tree',
+  program_name_hu: 'Serab Gyalcen rinpocse - Tanítók a menedékfán (15pm)',
+  program_name_en: 'Sherab Gyaltsen Rinpoche - The Teachers in the Refuge Tree',
   price_full: '3500',
   price_full_eur: 12.5,
   price_discount_eur: 6.5,
@@ -78,8 +79,8 @@ Program.create(id: 7,
   )
 # . . .
 Program.create(id: 8,
-  program_name_hu: 'Lama Ole Nydahl - tanitas este',
-  program_name_en: 'Lama Ole Nydahl - evening lecture',
+  program_name_hu: 'Láma Ole Nydahl tanítása (20pm)',
+  program_name_en: 'Lama Ole Nydahl\'s lecture',
   price_full: '3500',
   price_full_eur: 12.5,
   price_discount_eur: 6.5,
@@ -89,8 +90,8 @@ Program.create(id: 8,
   )
 # . . .
 Program.create(id: 9,
-  program_name_hu: 'Sherab Gyaltsen Rinpoche - Szobeli atadas (lungok)',
-  program_name_en: 'Lama Sherab Gyaltsen Rinpoche - Oral tranmission (lungs)',
+  program_name_hu: 'Serab Gyalcen rinpocse - Szóbeli átadas (lungok) (15pm)',
+  program_name_en: 'Sherab Gyaltsen Rinpoche - Oral tranmission (lungs)',
   price_full: '3500',
   price_full_eur: 12.5,
   price_discount_eur: 6.5,
@@ -100,8 +101,8 @@ Program.create(id: 9,
   )
 # . . .
 Program.create(id: 10,
-  program_name_hu: 'Lama Ole Nydahl - tanitas este',
-  program_name_en: 'Lama Ole Nydahl - evening lecture',
+  program_name_hu: 'Láma Ole Nydahl tanítása (20pm)',
+  program_name_en: 'Lama Ole Nydahl\'s lecture',
   price_full: '3500',
   price_full_eur: 12.5,
   price_discount_eur: 6.5,
@@ -111,8 +112,8 @@ Program.create(id: 10,
   )
 # . . .
 Program.create(id: 11,
-  program_name_hu: 'Serab Gyalcen rinpocse - Karma Paksi (2. Karmapa)-beavatas',
-  program_name_en: 'Sherab Gyaltsen Rinpoche - Karmapa Pakshi (2nd Karmapa) empowerment in the morning',
+  program_name_hu: 'Serab Gyalcen rinpocse - Karma Paksi (2. Karmapa)-beavatás (11am)',
+  program_name_en: 'Sherab Gyaltsen Rinpoche - Karmapa Pakshi (2nd Karmapa) empowerment',
   price_full: 7000,
   price_full_eur: 25,
   price_discount_eur: 12.5,
