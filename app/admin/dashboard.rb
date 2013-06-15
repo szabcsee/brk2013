668d49ee888b88a52edd8ea85daf381d215b72af
Szabcsee MacBook <szabi@digitalyogis.com>
2013-06-15 12:58:11 +0200

    New admin meal report

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -12,22 +12,20 @@ ActiveAdmin.register_page "Dashboard" do
 
     # Here is an example of a simple dashboard with columns and panels.
     #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
+     columns do
+       column do
+         panel "Special Reports" do
+           ul do
+               li link_to('Meal Reports', '/admin/meals/meal_report/')
+           end
+         end
+       end
 
-    #   column do
-    #     panel "Info" do
-    #       para "Welcome to ActiveAdmin."
-    #     end
-    #   end
-    # end
+       column do
+         panel "Info" do
+           para "Welcome to ActiveAdmin."
+         end
+       end
+     end
   end # content
 end
--- app/admin/meals.rb
+++ app/admin/meals.rb
@@ -1,4 +1,10 @@
 ActiveAdmin.register Meal do
+ filter :user, :collection => proc {(User.all).map{|c| [c.reference_number, c.id]}}
 
+ collection_action :meal_report do 
+
+ 	@meal_dates = ["2013-07-08","2013-07-09","2013-07-10","2013-07-11","2013-07-12","2013-07-13","2013-07-14"]
+    # This will render app/views/admin/meals/meal_report.html.erb
+  end
 
 end
